abd4cc77af23c6d0bb48f7b83dc7dba71aae9aa4
Akaash Dhayalan <akaashdhayalan243@gmail.com>
2025-01-12 15:36:14 +0800

    Set up index.js

---
+++ app/index.jsx
@@ -0,0 +1,22 @@
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Hi Dhanya!</Text>
+      <StatusBar style="auto" />
+      <Link href="/profile" style={{color: 'blue'}}> Profile</Link>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
