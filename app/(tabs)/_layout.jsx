bcef820f082a26beb31d31f804bf854f53628c32
Dhanya <sridhanya3000@gmail.com>
2025-01-13 16:33:21 +0800

    added calendar assets

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -5,12 +5,12 @@ import { icons } from '../../constants';
 
 const TabIcon = ({ icon, color, name, focused }) => {
   return (
-    <View className="flex items-center justify-center gap-2">
+    <View className="flex items-center justify-down gap-2">
       <Image
         source={icon}
         resizeMode="contain"
         tintColor={color}
-        className="w-6 h-6"
+        style={{ width: 20, height: 20 }}
       />
       <Text
         className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
@@ -34,7 +34,7 @@ const TabsLayout = () => {
             backgroundColor: "#161622",
             borderTopWidth: 1,
             borderTopColor: "#232533",
-            height: 84,
+            height: 80,
           },
         }}
       >
