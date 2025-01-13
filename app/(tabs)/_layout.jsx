ac59537aa27202e73409080e39d20d4b4d8dfa63
Akaash Dhayalan <akaashdhayalan243@gmail.com>
2025-01-13 15:01:33 +0800

    Fixed navigation bar

--- app/(tabs)/_layout.jsx
+++ app/(tabs)/_layout.jsx
@@ -5,15 +5,16 @@ import { icons } from '../../constants';
 
 const TabIcon = ({ icon, color, name, focused }) => {
   return (
-    <View className="flex items-center justify-center gap-2">
+    <View className="flex items-center justify-end gap-2"
+    style={{ width: 100, height: 100 }}>
       <Image
         source={icon}
         resizeMode="contain"
         tintColor={color}
-        className="w-6 h-6"
+        style={{ width: 20, height: 20, tintColor: color }}
       />
       <Text
-        className={`${focused ? "font-psemibold" : "font-pregular"} text-xs`}
+        className={`text-s ${focused ? "font-psemibold" : "font-pregular"}`}
         style={{ color: color }}
       >
         {name}
